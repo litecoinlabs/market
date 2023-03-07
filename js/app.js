5773a1c4158224e7c5d3b32d32a52212f8409a3f
jamesscaur <9779954+jamesscaur@users.noreply.github.com>
2023-03-08 00:32:53 +1300

    Make coin ticker dynamic (preset to LTC)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const coin = "LTC";
 const ordinalsExplorerUrl = "https://ordinals.com";
 const baseMempoolUrl = "https://mempool.space";
 const networkName = "mainnet";
@@ -90,8 +91,8 @@ async function selectUtxos(utxos, amount, vins, vouts, recommendedFeeRate) {
 
   if (selectedAmount < amount) {
     throw new Error(`Not enough cardinal spendable funds.
-Address has:  ${satToBtc(selectedAmount)} BTC
-Needed:          ${satToBtc(amount)} BTC
+Address has:  ${satToBtc(selectedAmount)} ${coin}
+Needed:          ${satToBtc(amount)} ${coin}
 
 UTXOs:
 ${utxos.map((x) => `${x.txid}:${x.vout}`).join("\n")}`);
@@ -448,7 +449,7 @@ async function* getLatestOrders(limit, nostrLimit = 20, filters = {}) {
       const ord = {
         title: `Buy for ${satToBtc(
           validatedPrice
-        )} BTC ($${satsToFormattedDollarString(
+        )} ${coin} ($${satsToFormattedDollarString(
           validatedPrice,
           await bitcoinPrice
         )})`,
@@ -781,7 +782,7 @@ async function inscriptionPage() {
   document.getElementById("utxo").value = inscription.output;
 
   const utxoValue = satToBtc(inscription["output value"]);
-  document.getElementById("utxoValue").value = `${utxoValue} BTC`;
+  document.getElementById("utxoValue").value = `${utxoValue} ${coin}`;
   document.getElementById("utxoValue").value += ` ($${(
     utxoValue * (await bitcoinPrice)
   ).toFixed(2)})`;
@@ -817,7 +818,7 @@ async function inscriptionPage() {
       const sellerOutput = sellerSignedPsbt.txOutputs[0];
       price = sellerOutput.value;
       const sellerOutputValueBtc = satToBtc(price);
-      const sellPriceText = `${sellerOutputValueBtc} BTC ($${(
+      const sellPriceText = `${sellerOutputValueBtc} ${coin} ($${(
         sellerOutputValueBtc * (await bitcoinPrice)
       ).toFixed(2)})`;
       document.getElementById("btnBuyInscriptionNow").style.display = "revert";
@@ -1242,11 +1243,11 @@ async function inscriptionPage() {
 
     if (changeValue < 0) {
       throw `Your wallet address doesn't have enough funds to buy this inscription.
-Price:          ${satToBtc(price)} BTC
-Fees:       ${satToBtc(fee + dummyUtxoValue)} BTC
-You have:   ${satToBtc(totalPaymentValue)} BTC
-Required:   ${satToBtc(totalValue - changeValue)} BTC
-Missing:     ${satToBtc(-changeValue)} BTC`;
+Price:          ${satToBtc(price)} ${coin}
+Fees:       ${satToBtc(fee + dummyUtxoValue)} ${coin}
+You have:   ${satToBtc(totalPaymentValue)} ${coin}
+Required:   ${satToBtc(totalValue - changeValue)} ${coin}
+Missing:     ${satToBtc(-changeValue)} ${coin}`;
     }
 
     // Change utxo
@@ -1306,7 +1307,7 @@ See transaction details on <a href="${baseMempoolUrl}/tx/${txId}" target="_blank
     }
 
     const sellerOutputValueBtc = satToBtc(price);
-    const sellPriceText = `${sellerOutputValueBtc} BTC ($${(
+    const sellPriceText = `${sellerOutputValueBtc} ${coin} ($${(
       sellerOutputValueBtc * (await bitcoinPrice)
     ).toFixed(2)})`;
     await displayBuyPsbt(
