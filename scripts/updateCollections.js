09d0734e4fb0dae71561dd2160fadb95f51ca61e
jamesscaur <9779954+jamesscaur@users.noreply.github.com>
2023-03-08 05:52:36 +1300

    Replace collection source and defaults

--- scripts/updateCollections.js
+++ scripts/updateCollections.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 async function main() {
   const collections = [];
   const collectionsRepoFiles = await fetch(
-    "https://api.github.com/repos/ordinals-wallet/ordinals-collections/git/trees/HEAD?per_page=1&recursive=1"
+    "https://api.github.com/repos/litecoinlabs/collections/git/trees/HEAD?per_page=1&recursive=1"
   ).then((r) => r.json());
   const collectionSlugs = collectionsRepoFiles.tree
     .map((t) => t.path.match(/collections\/(.*?)\/meta.json/)?.[1])
@@ -16,7 +16,7 @@ async function main() {
   for (const slug of collectionSlugs) {
     console.log(`${++counter}/${collectionSlugs.length} - ${slug}`);
     const collection = await fetch(
-      `https://raw.githubusercontent.com/ordinals-wallet/ordinals-collections/main/collections/${slug}/meta.json`
+      `https://raw.githubusercontent.com/litecoinlabs/collections/main/collections/${slug}/meta.json`
     ).then((r) => r.json());
 
     collections.push(collection);
