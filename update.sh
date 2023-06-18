611cf8eff23990b675e46c0dfc6116d08076aea6
“James <9779954+jamesscaur@users.noreply.github.com>
2023-06-18 23:02:00 +0000

    Only apply cache buster to root HTML files

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ cd ~/litecoinlabs/market
 git fetch
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
   git pull
-  VERSION=`date +%s`
-  find . -name "*.html" -exec sed -i "s/js\/app.js?v=[0-9]*/js\/app.js?v=$VERSION/g" {} \;
+  VERSION=$(date +%s)
+  for file in *.html; do sed -i "s/js\/app.js?v=[0-9]\+/js\/app.js?v=$VERSION/g" "$file"; done
   sudo cp -r * /usr/share/nginx/html/
   sudo service nginx restart
 fi
