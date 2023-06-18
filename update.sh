0e4f88cb65a8855f25c88bd39e38d846e6da9181
“James <9779954+jamesscaur@users.noreply.github.com>
2023-06-18 23:11:29 +0000

    Add CSS override

--- update.sh
+++ update.sh
@@ -4,7 +4,10 @@ git fetch
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
   git pull
   VERSION=$(date +%s)
-  for file in *.html; do sed -i "s/js\/app.js?v=[0-9]\+/js\/app.js?v=$VERSION/g" "$file"; done
+  for file in *.html; do 
+    sed -i "s/js\/app.js?v=[0-9]\+/js\/app.js?v=$VERSION/g" "$file"
+    sed -i "s/css\/style.css?v=[0-9]\+/css\/style.css?v=$VERSION/g" "$file"
+  done
   sudo cp -r * /usr/share/nginx/html/
   sudo service nginx restart
 fi
