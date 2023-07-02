df39e79eb684529e333e66af61ca2f065974a84e
“James <9779954+jamesscaur@users.noreply.github.com>
2023-07-03 00:15:57 +1200

    Remove cache buster

--- update.sh
+++ update.sh
@@ -9,15 +9,6 @@ if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
   echo "Local repository is not up-to-date. Updating now..."
   git reset --hard origin/main
 
-  echo "Generating a new version number..."
-  VERSION=$(date +%s)
-
-  echo "Updating version numbers in .html files..."
-  for file in *.html; do 
-    sed -i "s/js\/app.js?v=[0-9]\+/js\/app.js?v=$VERSION/g" "$file"
-    sed -i "s/css\/style.css?v=[0-9]\+/css\/style.css?v=$VERSION/g" "$file"
-  done
-
   echo "Setting execute permissions on update.sh..."
   chmod +x update.sh
 
